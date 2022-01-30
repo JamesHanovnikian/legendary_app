da00cc670d4cf0c821b3acddffd1a084131a5bb6
YOUR NAME <jhanov@gmail.com>
2022-01-30 15:30:41 -0600

    adds products object

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      {{ product["name"] }}
+    </div>
+    
+    
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Sign Up",
+      products: [
+        {
+          name: "trial",
+          description: "seven days",
+          price: 0,
+          subscriptionLength: "seven days",
+        },
+        {
+          name: "month",
+          description: "subscription month by month",
+          price: 5,
+          subscriptionLength: "month",
+        },
+        {
+          name: "year",
+          description: "subscription by year",
+          price: 40,
+          subscriptionLength: "yearly",
+        },
+      ],
+    };
+  },
+  created: function () {},
+  methods: {},
+};
 </script>
