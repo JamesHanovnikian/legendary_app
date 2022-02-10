719b4aa42ac1a5b36783d4f299519cc7bc622ea9
YOUR NAME <jhanov@gmail.com>
2022-02-09 20:20:58 -0600

    fix side panel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <div class="container">
     <main class="signup-grid">
       <div class="header">
         <h1> Sign Up</h1>
@@ -8,49 +9,49 @@
         <div class="card">
           <div class="card-body">
             <form>
-             <div class="form-group">
+             <div class="form-group input-group-sm">
                 <label for="exampleInputEmail1">Username</label>
                 <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
               </div>
-              <div class="form-group">
+              <div class="form-group input-group-sm ">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
               </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
+              <div class="form-group input-group-sm ">
+                <label class="justify-content-start" for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
               </div>
               <div class="row">
                 <div class="col-6">
-                  <div class="form-group">
+                  <div class="form-group input-group-sm ">
                     <label for="exampleInputPassword1">First Name</label>
                     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="First Name">
                   </div>
                 </div>
                 <div class="col-6">
-                  <div class="form-group">
+                  <div class="form-group input-group-sm ">
                     <label for="exampleInputPassword1"> Last Name</label>
                     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Last Name">
                   </div>
                 </div>
               </div>
               <div class="row">
-                <div class="col-6">
-                  <div class="form-group">
+                <div class="col-6 ">
+                  <div class="form-group input-group-sm ">
                     <label for="exampleInputPassword1"> Address </label>
                     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Address">
                   </div>
                 </div>
                 <div class="col-3">
-                  <div class="form-group">
+                  <div class="form-group input-group-sm ">
                     <label for="exampleInputPassword1"> State </label>
                     <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Last Name">
                   </div>
                 </div>
                 <div class="col-3">
-                  <div class="form-group">
-                    <label for="exampleInputPassword1"> Country </label>
+                  <div class="form-group input-group-sm ">
+                    <label class="control-label"> Country </label>
                     <input type="dropdown" class="form-control" id="exampleInputPassword1" placeholder="">
                   </div>
                 </div>
@@ -69,8 +70,10 @@
       <div class="subscription-options">
         <div v-for="product in products">
           <div class="product-box">
-            <h2> {{ product.name }}</h2>
-            <p> <span class="price"> {{ product.price }}</span> /month </p>
+            <h3 class="title"> {{ product.name }}</h3>
+            <hr>
+             <i class="fas fa-baseball-ball fa-2x "></i>
+            <p> <span class="price"> $ {{ product.price }}</span> /month </p>
             <p> {{ product.description }} </p>
             <button class="btn btn-primary"> Select </button>
           </div>
@@ -99,7 +102,7 @@
           </div> -->
 
     </main>
-    
+    </div>
     
           
           
@@ -143,23 +146,50 @@
   grid-column-start: 1;
   grid-column-end: 3;
   grid-row-start: 2;
+  grid-row-end: 4;
+}
+
+.registration-form .card {
+  border: 1.5px solid rgb(146, 146, 146, 0.8);
+}
+
+.registration-form .form-control {
+  border-radius: 8px;
+  font-size: 10px;
+}
+
+.registration-form label {
+  font-size: 12px;
 }
 
 .subscription-options {
   grid-column-start: 3;
   grid-column-end: 5;
+  grid-row-start: 1;
   color: #fff;
+}
+
+.subscription-options .title {
+  text-transform: uppercase;
 }
 
 .subscription-options .product-box {
   border-radius: 3rem;
-  border: 2px solid #fff;
-  width: 75%;
+  border: 1.5px solid rgb(146, 146, 146, 0.8);
+  width: 65%;
   padding: 4.5px;
+  margin: 15px;
+  background-color: #fff;
+  color: #333;
+}
+
+.subscription-options label {
+  font-size: 8px;
 }
 
 form {
   padding: 10px;
+  width: 75%;
 }
 
 form .form-group input {
@@ -174,11 +204,15 @@ h1 {
   font-weight: 600;
 }
 
+h3 {
+  font-size: 18px;
+}
+
 .card-title {
   font-size: 1.2rem;
 }
 .price {
-  font-size: 1.75rem;
+  font-size: 1.25rem;
 }
 
 .subscription-options .card {
@@ -191,21 +225,23 @@ h1 {
 
 .btn {
   width: 30%;
+  border-radius: 3rem;
 }
 
 .card {
-  border-radius: 4rem;
+  border-radius: 2.5rem;
   border: none;
 }
 
 .card-text {
 }
 
-.subscription-options .card:hover {
+.subscription-options .product-box:hover {
   transform: scale(1.02, 1.02);
   box-shadow: 0 1rem 3rem rgba(0, 0, 0, 0.75) !important;
   will-change: transform;
   backface-visibility: hidden;
+  background-color: rgba(226, 221, 221, 0.8);
 }
 
 .card:hover::after {
